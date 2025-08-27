bfd069ce6ec967488846097fd0c9adf2ed1b8cfb
ricardoaamb <ricardo.barbosa@petzpartner.com.br>
2025-08-27 11:18:54 -0300

    feat: criação do backend inicial da nossa aplicação incluindo o desenho inicial do nosso banco de dados com o prisma.js

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PromptModule } from './prompt/prompt.module';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/prompt.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PromptModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
